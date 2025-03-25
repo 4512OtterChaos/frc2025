1415d4ed5b1e744165279db7c4c200310ed03fa8
4512 Otter Chaos <otterchaos4512@gmail.com>
2025-03-24 18:03:03 -0700

    Created funnel class using manipulator base

---
+++ src/main/java/frc/robot/subsystems/funnel/Funnel.java
@@ -0,0 +1,254 @@
+package frc.robot.subsystems.funnel;
+
+import static edu.wpi.first.wpilibj2.command.Commands.run;
+import static edu.wpi.first.wpilibj2.command.Commands.sequence;
+import static edu.wpi.first.wpilibj2.command.Commands.startEnd;
+
+import static frc.robot.subsystems.funnel.FunnelConstants.*;
+
+import com.ctre.phoenix6.BaseStatusSignal;
+import com.ctre.phoenix6.StatusSignal;
+import com.ctre.phoenix6.controls.PositionVoltage;
+import com.ctre.phoenix6.hardware.ParentDevice;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.sim.ChassisReference;
+
+import au.grapplerobotics.ConfigurationFailedException;
+import au.grapplerobotics.LaserCan;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Current;
+import edu.wpi.first.units.measure.Distance;
+import edu.wpi.first.units.measure.Voltage;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.util.PhoenixUtil;
+import frc.robot.util.TunableNumber;
+
+public class Funnel extends SubsystemBase{
+    private TalonFX motor = new TalonFX(kMotorID);
+    
+    // private SimpleMotorFeedforward ff = new SimpleMotorFeedforward(kConfig.Slot0.kS, kConfig.Slot0.kV);
+    // private PIDController PID = new PIDController(kConfig.Slot0.kP, kConfig.Slot0.kI, kConfig.Slot0.kD);
+
+    private PositionVoltage positionRequest = new PositionVoltage(0).withEnableFOC(false);
+
+    boolean isManual = true;
+    
+    double lastFreeTime = Timer.getFPGATimestamp();
+
+    private final StatusSignal<Voltage> voltageStatus = motor.getMotorVoltage();
+    private final StatusSignal<Angle> positionStatus = motor.getPosition();
+    private final StatusSignal<AngularVelocity> velocityStatus = motor.getVelocity();
+    private final StatusSignal<Current> statorStatus = motor.getStatorCurrent();
+
+    // Tunable numbers
+    private final TunableNumber feedSlowVoltage = new TunableNumber("Coral/feedSlowVoltage", kFeedSlowVolts);
+    private final TunableNumber feedFastVoltage = new TunableNumber("Coral/feedFastVoltage", kFeedFastVolts);
+
+    // private final TunableNumber rpmPerVolt = new TunableNumber("Coral/rpmPerVolt", kRPMPerVolt);
+
+
+    private final TunableNumber kP = new TunableNumber("Coral/kP", kConfig.Slot0.kP);
+    private final TunableNumber kD = new TunableNumber("Coral/kD", kConfig.Slot0.kD);
+
+    private final TunableNumber kS = new TunableNumber("Coral/kS", kConfig.Slot0.kS);
+    private final TunableNumber kV = new TunableNumber("Coral/kV", kConfig.Slot0.kV);
+    
+    
+    public Funnel(){
+        // try applying motor configs
+        boolean success = PhoenixUtil.tryUntilOk(5, () -> motor.getConfigurator().apply(kConfig));
+        if (!success) DriverStation.reportWarning("Failed applying Funnel motor configuration!", false);
+
+        voltageStatus.setUpdateFrequency(100);
+        positionStatus.setUpdateFrequency(100);
+        velocityStatus.setUpdateFrequency(50);
+        statorStatus.setUpdateFrequency(50);
+        ParentDevice.optimizeBusUtilizationForAll(motor);
+
+        SmartDashboard.putData("Funnel/Subsystem", this);
+    }
+
+    @Override
+    public void periodic() {
+        // if(!isManual) {
+        //     motor.setVoltage(ff.calculate(PID.getSetpoint())+PID.calculate(getVelocity().in(RotationsPerSecond)));
+        // }
+
+        if (getCurrent() <= kStallCurrent){
+            lastFreeTime = Timer.getFPGATimestamp();
+        }
+
+        changeTunable();
+
+        log();
+    }
+
+    public void setVoltage(double voltage){
+        isManual = true;
+        motor.setVoltage(-voltage);
+    }
+
+    public void setTargetPos(Angle position) {
+        isManual = false;
+        motor.setControl(positionRequest.withPosition(position));
+    }
+
+    // public void setVelocity(double RPM) {
+    //     isManual = false;
+    //     PID.setSetpoint(RPM);
+    // }
+
+    public Angle getPosition() {
+        return positionStatus.getValue();
+    }
+
+    public AngularVelocity getVelocity(){
+        return velocityStatus.getValue();
+    }
+
+    public double getCurrent(){
+        return statorStatus.getValueAsDouble();
+    }
+
+    public Trigger isStalled(){
+        return new Trigger(() -> Timer.getFPGATimestamp() >= (lastFreeTime + kStallTime));
+    }
+
+    public Trigger isCoralDetected(){ //TODO: Use velocity detection
+        return new Trigger(() -> {
+            var measurement = sensor.getMeasurement();
+            if (measurement == null) return false;
+            boolean withinRange = measurement.distance_mm <= kSensorMaxCoralDist.in(Millimeters);
+            boolean validRange = measurement.distance_mm > 0;
+            boolean statusOk = measurement.status == LaserCan.LASERCAN_STATUS_VALID_MEASUREMENT;
+            return withinRange && validRange && statusOk;
+        });
+    }
+
+    public Command setVoltageC(double voltage){
+        return run(()->setVoltage(voltage));
+    }
+
+    public Command feedCoralSlowC() {
+        return run(()->setVoltage(feedSlowVoltage.get())).withName("FeedCoralSlow");
+    }
+
+    public Command feedCoralFastC() {
+        return run(()->setVoltage(feedFastVoltage.get())).withName("FeedCoralFast");
+    }
+
+    public Command backfeedCoralSlowC() {
+        return run(()->setVoltage(-feedSlowVoltage.get())).withName("BackfeedCoralSlow");
+    }
+
+    public Command scoreCoralC() {
+        return run(()->setVoltage(scoreCoralVolts.get())).withName("ScoreCoral");
+    }
+
+    public Command holdAlgaeC() {
+        return run(()->setVoltage(holdAlgaeVolts.get())).withName("HoldAlgae");
+    }
+
+    public Command scoreAlgaeC() {
+        return run(()->setVoltage(scoreAlgaeVolts.get())).withName("ScoreAlgae");
+    }
+
+    public Command holdCoralVoltsC(){
+        return sequence(
+            setVoltageC(0).withTimeout(0.5),
+            setVoltageC(-0.5).withTimeout(0.5)
+            ).repeatedly().withName("D:HoldCoral");
+    }
+
+    public Command feedCoralSequenceC() {
+        // return feedCoralSlowC().until(isCoralDetected().negate()).withName("FeedCoral");
+
+        // return sequence(
+        //     feedCoralFastC().withTimeout(0.25),
+        //     feedCoralSlowC()
+        // ).until(isCoralDetected().negate()).withName("FeedCoral");
+
+        return sequence( // Grab coral quickly, then slowly home it to a consistent position
+            feedCoralFastC().until(isCoralDetected().negate()),
+            backfeedCoralSlowC().until(isCoralDetected()),
+            feedCoralSlowC().until(isCoralDetected().negate())
+        ).withName("FeedCoral");
+    }
+
+    public Command feedCoralFastSequenceC() {
+        return sequence(
+            feedCoralFastC().until(isCoralDetected()),
+            feedCoralSequenceC()
+        );
+    }
+
+    /** Does not end */
+    public Command setPositionC(Angle position) {
+        return run(() -> setTargetPos(position)).withName("SetPosition");
+    }
+
+    public Command holdPositionC() {
+        return startEnd(
+            () -> setTargetPos(getPosition()),
+            () -> {}
+        ).withName("HoldPosition");
+    }
+
+    private void changeTunable() {
+        feedSlowVoltage.poll();
+        feedFastVoltage.poll();
+        scoreCoralVolts.poll();
+        holdAlgaeVolts.poll();
+        scoreAlgaeVolts.poll();
+
+        kP.poll();
+        kD.poll();
+        kS.poll();
+        kV.poll();
+
+        int hash = hashCode();
+        // PID
+        if (kP.hasChanged(hash) || kD.hasChanged(hash) || kS.hasChanged(hash) || kV.hasChanged(hash)) {
+            kConfig.Slot0.kP = kP.get();
+            kConfig.Slot0.kD = kD.get();
+            kConfig.Slot0.kS = kS.get();
+            kConfig.Slot0.kV = kV.get();
+            motor.getConfigurator().apply(kConfig.Slot0);
+        }
+    }
+
+    private void log() {
+        BaseStatusSignal.refreshAll(voltageStatus, positionStatus, velocityStatus, statorStatus);
+        SmartDashboard.putNumber("Coral/Motor Voltage", voltageStatus.getValueAsDouble());
+        SmartDashboard.putNumber("Coral/Motor Current", getCurrent());
+        SmartDashboard.putNumber("Coral/Rotations", getPosition().in(Rotations));
+        SmartDashboard.putNumber("Coral/Rotations per second", getVelocity().in(RotationsPerSecond));
+        SmartDashboard.putBoolean("Coral/isStalled", isStalled().getAsBoolean());
+
+        SmartDashboard.putNumber("Coral/Coral Travelled Inches", kCoralRollerDia.times(Math.PI).per(Rotation).timesDivisor(getPosition()).in(Inches));
+    }
+    
+
+    //########## Simulation
+
+    @Override
+    public void simulationPeriodic() {
+        var motorSim = motor.getSimState();
+        motorSim.Orientation = ChassisReference.CounterClockwise_Positive;
+        motorSim.setSupplyVoltage(RobotController.getBatteryVoltage());
+
+        model.setInput(-motorSim.getMotorVoltage());
+        model.update(0.02);
+
+        motorSim.setRawRotorPosition(model.getAngularPosition().times(kGearRatio));
+        motorSim.setRotorVelocity(model.getAngularVelocity().times(kGearRatio));
+        motorSim.setRotorAcceleration(model.getAngularAcceleration().times(kGearRatio));
+    }
+}
---
+++ src/main/java/frc/robot/subsystems/funnel/FunnelConstants.java
@@ -0,0 +1,67 @@
+package frc.robot.subsystems.funnel;
+
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+import com.ctre.phoenix6.configs.FeedbackConfigs;
+import com.ctre.phoenix6.configs.MotorOutputConfigs;
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
+import au.grapplerobotics.interfaces.LaserCanInterface.RangingMode;
+import au.grapplerobotics.interfaces.LaserCanInterface.RegionOfInterest;
+import au.grapplerobotics.interfaces.LaserCanInterface.TimingBudget;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.system.plant.LinearSystemId;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.units.measure.Distance;
+import static edu.wpi.first.units.Units.*;
+import edu.wpi.first.wpilibj.simulation.DCMotorSim;
+
+public class FunnelConstants {
+    public static int kMotorID = 41;
+
+    public static DCMotor kMotor = DCMotor.getKrakenX60(1);
+
+    public static double kFeedFastVolts = 0.9;
+    public static double kFeedSlowVolts = 0.5;
+
+    public static int kGearRatio = 3;
+    public static Distance kCoralRollerDia = Inches.of(2);
+
+    public static final int kMotorStallLimit = 40;
+
+    public static final double kMotorStallDetection = 15;
+    
+    public static double kStallCurrent = 20;
+    public static double kStallTime = 0.3;
+
+
+    public static final TalonFXConfiguration kConfig = new TalonFXConfiguration();
+    static {
+        FeedbackConfigs feedback = kConfig.Feedback;
+        feedback.SensorToMechanismRatio = kGearRatio;
+        //          motor rotations  -->  shaft rotations
+
+        MotorOutputConfigs output = kConfig.MotorOutput;
+        output.NeutralMode = NeutralModeValue.Brake;
+        output.Inverted = InvertedValue.Clockwise_Positive;
+
+        CurrentLimitsConfigs current = kConfig.CurrentLimits;
+        current.StatorCurrentLimitEnable = true;
+        current.StatorCurrentLimit = 40;
+
+        Slot0Configs control = kConfig.Slot0; // Position PID
+        control.kP = 20;
+        control.kI = 0;
+        control.kD = 0;
+
+        control.kS = 0.25;
+        control.kV = 1.0 / Units.radiansToRotations(kMotor.withReduction(kGearRatio).KvRadPerSecPerVolt);
+    }
+
+    public static final DCMotorSim model = new DCMotorSim(
+        LinearSystemId.createDCMotorSystem(1.3 * 1.0 / kMotor.withReduction(kGearRatio).KvRadPerSecPerVolt, 0.002),
+        kMotor
+    );
+}
