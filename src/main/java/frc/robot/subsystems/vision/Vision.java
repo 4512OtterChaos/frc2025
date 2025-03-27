1a060676651a281814e2b52e30a26092ed0563e2
Nolan Brown <noleetarrr@gmail.com>
2025-03-27 00:24:56 -0700

    use two cameras

--- src/main/java/frc/robot/subsystems/vision/Vision.java
+++ src/main/java/frc/robot/subsystems/vision/Vision.java
@@ -8,8 +8,6 @@ import java.util.stream.Collectors;
 
 import org.photonvision.EstimatedRobotPose;
 import org.photonvision.PhotonCamera;
-import org.photonvision.PhotonPoseEstimator;
-import org.photonvision.PhotonPoseEstimator.PoseStrategy;
 import org.photonvision.simulation.PhotonCameraSim;
 import org.photonvision.simulation.SimCameraProperties;
 import org.photonvision.simulation.VisionSystemSim;
@@ -27,8 +25,7 @@ import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.interpolation.TimeInterpolatableBuffer;
 import edu.wpi.first.math.numbers.*;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.networktables.StructArrayPublisher;
@@ -36,16 +33,17 @@ import edu.wpi.first.networktables.StructPublisher;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.Robot;
 import frc.robot.util.TunableNumber;
 
 public class Vision {
     private final PhotonCamera cameraLeft;
     private final PhotonCamera cameraRight;
-    private final PhotonPoseEstimator photonEstimator;
-    private double lastEstTimestamp = 0;
+    private final TimeInterpolatableBuffer<Rotation2d> headingBuffer =
+            TimeInterpolatableBuffer.createBuffer(1.0);
+
+    private final StructPublisher<Pose3d> leftCamPose = NetworkTableInstance.getDefault().getStructTopic("Vision/Left Cam/RobotToCam Pose", Pose3d.struct).publish();
+    private final StructPublisher<Pose3d> rightCamPose = NetworkTableInstance.getDefault().getStructTopic("Vision/Right Cam/RobotToCam Pose", Pose3d.struct).publish();
 
     private final StructArrayPublisher<Pose3d> leftVisibleTagsPub = NetworkTableInstance.getDefault().getStructArrayTopic("Vision/Left Cam/Visible Tag Poses", Pose3d.struct).publish();
     private final StructPublisher<Pose3d> leftEstimatePosePub = NetworkTableInstance.getDefault().getStructTopic("Vision/Left Cam/Estimated Pose", Pose3d.struct).publish();
@@ -70,11 +68,6 @@ public class Vision {
         cameraLeft = new PhotonCamera(kCameraNameFacingLeft);
         cameraRight = new PhotonCamera(kCameraNameFacingRight);
 
-        photonEstimator =
-                new PhotonPoseEstimator(
-                        kTagLayout, PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR, kRobotToCamFacingLeft);
-        photonEstimator.setMultiTagFallbackStrategy(PoseStrategy.PNP_DISTANCE_TRIG_SOLVE);
-
         // ----- Simulation
         if (Robot.isSimulation()) {
             // Create the vision system simulation which handles cameras and targets on the field.
@@ -82,21 +75,29 @@ public class Vision {
             // Add all the AprilTags inside the tag layout as visible targets to this simulated field.
             visionSim.addAprilTags(kTagLayout);
             // Create simulated camera properties. These can be set to mimic your actual camera.
-            var cameraProp = new SimCameraProperties();
-            cameraProp.setCalibration(
+            var leftCamProp = new SimCameraProperties();
+            leftCamProp.setCalibration(
                 1280, 800,
                 MatBuilder.fill(Nat.N3(), Nat.N3(), 912.709493756531,0.0,604.8806144623338,0.0,912.1834423662752,417.52151705840043,0.0,0.0,1.0),
                 VecBuilder.fill(0.04876898702521537,-0.07988383118417577,5.014752423658081E-4,-8.445052005705895E-4,0.014724717037463421,-0.001568504911944983,0.0025829433227567843,-0.0013020935054274872)
             );
-            cameraProp.setCalibError(0.45, 0.10);
-            cameraProp.setFPS(40);
-            cameraProp.setAvgLatencyMs(30);
-            cameraProp.setLatencyStdDevMs(8);
+            leftCamProp.setCalibError(0.45, 0.10);
+            leftCamProp.setFPS(40);
+            leftCamProp.setAvgLatencyMs(30);
+            leftCamProp.setLatencyStdDevMs(8);
+
+            var rightCamProp = leftCamProp.copy();
+            rightCamProp.setCalibration(
+                1280, 800,
+                MatBuilder.fill(Nat.N3(), Nat.N3(), 901.0021345114463,0.0,664.4681708224546,0.0,900.9492509895897,426.3018086357822,0.0,0.0,1.0),
+                VecBuilder.fill(0.045596409600450645,-0.055142592429464926,-8.63711484780898E-5,-7.915368273629485E-4,-0.007584370524445873,-0.0012547761325422994,0.005980279080816732,0.00159536717183096)
+            );
+
             // Create a PhotonCameraSim which will update the linked PhotonCamera's values with visible
             // targets.
-            cameraSimFacingLeft = new PhotonCameraSim(cameraLeft, cameraProp);
+            cameraSimFacingLeft = new PhotonCameraSim(cameraLeft, leftCamProp);
             cameraSimFacingLeft.setMinTargetAreaPixels(400);
-            cameraSimFacingRight = new PhotonCameraSim(cameraRight, cameraProp);
+            cameraSimFacingRight = new PhotonCameraSim(cameraRight, leftCamProp);
             cameraSimFacingRight.setMinTargetAreaPixels(400);
             // Add the simulated camera to view the targets on this simulated field.
             visionSim.addCamera(cameraSimFacingLeft, kRobotToCamFacingLeft);
@@ -120,38 +121,35 @@ public class Vision {
         }
     }
 
+    /**
+     * Updates the given pose estimator with all unread vision measurements from the cameras.
+     * @param estimator
+     * @param fieldToRobotRot Heading of the robot on the field
+     * @param rotationTimestamp Timestamp the heading of the robot was captured at
+     */
     public void update(SwerveDrivePoseEstimator estimator, Rotation2d fieldToRobotRot, double rotationTimestamp) {
+        headingBuffer.addSample(rotationTimestamp, fieldToRobotRot);
+
         // Update estimator for new left-facing camera results
         var leftResults = cameraLeft.getAllUnreadResults();
-        if (leftResults.size() > 1) { // ensure new results are last
-            leftResults.sort((r1, r2) -> Double.compare(r1.getTimestampSeconds(), r2.getTimestampSeconds()));
-        }
-        for (var result : leftResults) {
-            estimatePoseGivenRot(result, fieldToRobotRot, kRobotToCamFacingLeft).ifPresent(estimate -> {
-                var stdDevs = getEstimationStdDevs(estimate.estimatedPose);
-                estimator.addVisionMeasurement(estimate.estimatedPose.toPose2d(), estimate.timestampSeconds, stdDevs);
-            });
-        }
+        processResults(leftResults, estimator, kRobotToCamFacingLeft).ifPresent(latestEstimate -> {
+            leftEstimatePosePub.set(latestEstimate);
+        });
 
         // Update estimator for new right-facing camera results
         var rightResults = cameraRight.getAllUnreadResults();
-        if (rightResults.size() > 1) { // ensure new results are last
-            rightResults.sort((r1, r2) -> Double.compare(r1.getTimestampSeconds(), r2.getTimestampSeconds()));
-        }
-        for (var result : rightResults) {
-            estimatePoseGivenRot(result, fieldToRobotRot, kRobotToCamFacingRight).ifPresent(estimate -> {
-                var stdDevs = getEstimationStdDevs(estimate.estimatedPose);
-                estimator.addVisionMeasurement(estimate.estimatedPose.toPose2d(), estimate.timestampSeconds, stdDevs);
-            });
-        }
+        processResults(rightResults, estimator, kRobotToCamFacingRight).ifPresent(latestEstimate -> {
+            rightEstimatePosePub.set(latestEstimate);
+        });
 
         // Grab updated pose for logging
         var updatedRobotPose = estimator.getEstimatedPosition();
         var updatedRobotPose3d = new Pose3d(updatedRobotPose);
+        leftCamPose.set(updatedRobotPose3d.plus(kRobotToCamFacingLeft));
+        rightCamPose.set(updatedRobotPose3d.plus(kRobotToCamFacingRight));
 
         // Log last left-facing camera estimate and visible tags
         if (leftResults.isEmpty()) {
-            leftEstimatePosePub.set(null);
             leftVisibleTagsPub.set(null);
         }
         else {
@@ -162,7 +160,6 @@ public class Vision {
 
         // Log last right-facing camera estimate and visible tags
         if (rightResults.isEmpty()) {
-            rightEstimatePosePub.set(null);
             rightVisibleTagsPub.set(null);
         }
         else {
@@ -172,35 +169,75 @@ public class Vision {
         }
     }
 
-    public PhotonPipelineResult getLatestResult() {
-        return cameraLeft.getLatestResult();
+    /**
+     * For all given camera results, add pose estimates to the given estimator.
+     * @param results
+     * @param estimator
+     * @param robotToCam
+     * @return The estimated robot pose for the last given result, or empty if none.
+     */
+    private Optional<Pose3d> processResults(List<PhotonPipelineResult> results, SwerveDrivePoseEstimator estimator, Transform3d robotToCam) {
+        Optional<Pose3d> latestCamEstimate = Optional.empty();
+
+        if (results.size() > 1) { // ensure new results are last
+            results.sort((r1, r2) -> Double.compare(r1.getTimestampSeconds(), r2.getTimestampSeconds()));
+        }
+        for (var result : results) {
+            Rotation2d rotAtResult = headingBuffer.getSample(result.getTimestampSeconds()).get();
+            var opt = estimatePoseGivenRot(result, rotAtResult, robotToCam);
+            if (opt.isEmpty()) continue;
+            EstimatedRobotPose estimate = opt.get();
+            latestCamEstimate = Optional.of(estimate.estimatedPose);
+            var stdDevs = getEstimationStdDevs(result, estimate.estimatedPose);
+            estimator.addVisionMeasurement(estimate.estimatedPose.toPose2d(), estimate.timestampSeconds, stdDevs);
+        }
+
+        return latestCamEstimate;
     }
 
+    /**
+     * Find the estimated robot pose for the given camera result.
+     * Ignores estimated rotation in favor of the given robot rotation, using only estimated translation and tag layout.
+     * @param result
+     * @param fieldToRobotRot
+     * @param robotToCam
+     * @return
+     */
     public Optional<EstimatedRobotPose> estimatePoseGivenRot(PhotonPipelineResult result, Rotation2d fieldToRobotRot, Transform3d robotToCam) {
-        var bestTarget = result.getBestTarget();
-        if (bestTarget == null) return Optional.empty();
-        var camToTargetTrl = bestTarget.getBestCameraToTarget().getTranslation();
+        /*
+         * We will always use a single tag for estimating the robot's pose. In the case that multiple tags
+         * are visible, the multitag calculation on-coprocessor should update all individual tag transforms,
+         * meaning any tag we choose should produce the same result.
+         */
+        // First, discard any visible tags we aren't tracking in our tag layout
+        var visibleTags = result.getTargets();
+        visibleTags.removeIf(target -> kTagLayout.getTagPose(target.fiducialId).isEmpty());
+        if (visibleTags.isEmpty()) return Optional.empty();
 
-        var tagPoseMaybe = kTagLayout.getTagPose(bestTarget.fiducialId);
-        if (tagPoseMaybe.isEmpty()) return Optional.empty();
-        var tagPose = tagPoseMaybe.get();
+        var target = visibleTags.get(0);
+        var camToTargetTrl = target.getBestCameraToTarget().getTranslation();
+
+        /*
+         * Traditionally, we want to use the camera-to-tag transform off of the tag layout pose to find the camera's
+         * pose in the field. In this case, we will replace the transform's rotation with our given robot rotation
+         * to reduce noise in the estimate translation caused by high noise in the estimated rotation.
+         */
+        var tagPose = kTagLayout.getTagPose(target.fiducialId).get();
         Rotation3d tagRot = tagPose.getRotation();
         var fieldToCamRot = robotToCam.getRotation().rotateBy(new Rotation3d(fieldToRobotRot));
         var targetToCamTrl = camToTargetTrl.unaryMinus().rotateBy(
                 fieldToCamRot.minus(tagRot));
 
-        var targetToCam = new Transform3d(
-            tagPose,
-            new Pose3d(
-                tagPose.getTranslation().plus(targetToCamTrl.rotateBy(tagPose.getRotation())),
-                fieldToCamRot
-            )
+        Pose3d fieldToCam = new Pose3d(
+            tagPose.getTranslation().plus(targetToCamTrl.rotateBy(tagPose.getRotation())),
+            fieldToCamRot
         );
 
+
         var estimate = new EstimatedRobotPose(
-            tagPose.plus(targetToCam).plus(robotToCam.inverse()),
+            fieldToCam.plus(robotToCam.inverse()),
             result.getTimestampSeconds(),
-            List.of(bestTarget),
+            result.getTargets(),
             null
         );
         return Optional.of(estimate);
@@ -213,14 +250,13 @@ public class Vision {
      *
      * @param estimatedPose The estimated pose to guess standard deviations for.
      */
-    public Matrix<N3, N1> getEstimationStdDevs(Pose3d estimatedPose) {
+    public Matrix<N3, N1> getEstimationStdDevs(PhotonPipelineResult result, Pose3d estimatedPose) {
         var estStdDevs = lowTrustStdDevs;
-        var result = getLatestResult();
         var targets = result.getTargets();
         int numTags = 0;
         double avgDist = 0;
         for (var tgt : targets) {
-            var tagPose = photonEstimator.getFieldTags().getTagPose(tgt.getFiducialId());
+            var tagPose = kTagLayout.getTagPose(tgt.getFiducialId());
             if (tagPose.isEmpty()) continue;
             numTags++;
             avgDist +=
